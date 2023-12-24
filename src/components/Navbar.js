34d9d69e1678bdcfb109c6296a6ff43735220a65
torbenconto <torben@2x1.dev>
2023-12-24 18:36:40 -0500

    fonts.css, start navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import "../fonts.css"
+
+function Navbar() {
+  return (
+    <div className="w-full">
+        <div className="p-12">
+            <h1 className="text-xl abhaya-libre-normal">Torben Conto</h1>
+        </div>
+    </div>
+  );
+}
+
+export default Navbar;
