2ab37b6aacd0e0666a7aff3c6f38c6cb831c1327
torbenconto <torben@2x1.dev>
2023-12-24 19:24:25 -0500

    test navbar, fix fonts

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,36 @@
+import { useState } from "react";
+
 import "../fonts.css"
+import "./Navbar.css"
 
 function Navbar() {
+
+  const [activeLink, setActiveLink] = useState(0);
+
+  const handleNavClick = (index) => {
+    setActiveLink(index);
+  };
+
+
   return (
     <div className="w-full">
-        <div className="p-12">
-            <h1 className="text-xl abhaya-libre-normal">Torben Conto</h1>
+        {/* Bar with padding */}
+        <div className="p-12 flex">
+            <h1 className="w-1/2 text-xl abhaya-libre-extrabold">Torben Conto</h1>
+            {/* Nav Options */}
+            <div className="w-1/2 flex justify-center">
+              <div className="w-1/2 flex abhaya-libre-medium text-md items-center justify-around">
+                {["Home", "About", "Projects", "Contact"].map((label, index) => (
+                <a
+                  key={index}
+                  className={`nav-option ${activeLink === index ? "active" : ""}`}
+                  onClick={() => handleNavClick(index)}
+                >
+                  {label}
+                </a>
+              ))}
+              </div>
+            </div>
         </div>
     </div>
   );
