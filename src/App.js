aef7f97d624bd948f7ffe3fa1b35a19b95d37f18
torbenconto <torben@2x1.dev>
2023-12-31 00:54:35 -0500

    start complex styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
 import HomeText from "./components/HomeText";
+import TopGradient from "./components/TopGradient";
 
 function App() {
     return (
         <>
             <div className="flex flex-col h-screen p-4 md:px-12">
+                <TopGradient/>
                 <Navbar />
                 <div className="flex-1 flex flex-col justify-end md:w-1/3">
                     <HomeText />
---
+++ src/components/TopGradient.js
@@ -0,0 +1,10 @@
+import "./TopGradient.css"
+function TopGradient() {
+  return (
+    <div className="box">
+        <img className="ellipse" alt="Ellipse" src="/ellipse-2.png"/>
+    </div>
+  );
+}
+
+export default TopGradient;
