7e616d59ca5fead14a10d06c2a38b815468fedd0
torbenconto <torben@2x1.dev>
2023-12-31 21:59:14 -0500

    Set max-h-screen

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
     }, []);
 
     return (
-        <div className="flex flex-col h-screen p-4 md:px-12">
+        <div className="flex flex-col h-screen max-h-screen p-4 md:px-12">
             <TopGradient/>
             <Navbar />
             <div className={`flex-1 flex flex-col mb-12 justify-end md:w-1/3 ${loading ? 'hidden' : 'block'}`}>
