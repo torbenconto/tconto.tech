ff622904abc6a82b8f582dbd9ec6ef0c08eb0404
torbenconto <torben@2x1.dev>
2023-12-31 22:10:40 -0500

    test

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
         <div className="flex flex-col h-screen max-h-screen p-4 md:px-12">
             <TopGradient/>
             <Navbar />
-            <div className={`flex-1 flex flex-col mb-12 justify-end md:w-1/3 ${loading ? 'hidden' : 'block'}`}>
+            <div className={`flex-1 flex flex-col md:mb-12 justify-end md:w-1/3 ${loading ? 'hidden' : 'block'}`}>
                 <SideGradient/>
                 <MiddleGradient/>
                 <HomeText />
